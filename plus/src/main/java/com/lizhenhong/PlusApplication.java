92fadb17ec374216752a3d0de5e4050f8cd70e11
lizhenhong <18732068538@163.com>
2020-08-15 17:13:27 +0800

    创建mybatis-plus工程

---
+++ plus/src/main/java/com/lizhenhong/PlusApplication.java
@@ -0,0 +1,13 @@
+package com.lizhenhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusApplication.class, args);
+    }
+
+}
